6a98166506975f0c7b11368f783c62e6fb3d0872
showrin20 <showrin.rahman@g.bracu.ac.bd>
2023-12-05 00:37:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2>Hello This is netflix clone</h2>
+    </>
+  );
+}
+
+export default App;
